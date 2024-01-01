5824ca7a59ba2726347b5d851d83303a73bf1efb
AntorFR <contact@antor.fr>
2024-01-01 23:12:15 +0100

    Add color option for Stars effect

--- components/custom-addressable-effetcs/__init__.py
+++ components/custom-addressable-effetcs/__init__.py
@@ -4,16 +4,24 @@ from esphome.components.light.types import AddressableLightEffect
 from esphome.components.light.effects import register_addressable_effect
 
 from esphome.const import (
-    CONF_NAME
+    CONF_NAME,
+    CONF_BRIGHTNESS,
+    CONF_RED,
+    CONF_GREEN,
+    CONF_BLUE,
+    CONF_WHITE,
 )
 
+CONF_COLOR = "color"
 CONF_STARS_PROBABILITY = "stars_probability"
 CONF_CHRISTMASS_BIT_SIZE = "bit_size"
 CONF_CHRISTMASS_BLANK_SIZE = "blank_size"
 
 light_ns = cg.esphome_ns.namespace("light")
 AddressableStarsEffect = light_ns.class_("AddressableStarsEffect", AddressableLightEffect)
+AddressableColorStarsEffectColor = light_ns.struct("AddressableColorStarsEffectColor")
 AddressableChristmasEffect = light_ns.class_("AddressableChristmasEffect", AddressableLightEffect)
+
 
 CONFIG_SCHEMA = cv.All(cv.Schema({}), cv.only_with_arduino)
 
@@ -23,11 +31,31 @@ CONFIG_SCHEMA = cv.All(cv.Schema({}), cv.only_with_arduino)
     "Stars",
     {
         cv.Optional(CONF_STARS_PROBABILITY, default="10%"): cv.percentage,
+        cv.Optional(
+            CONF_COLOR, default=[{CONF_BRIGHTNESS: 0.0}],
+        ): cv.ensure_list(
+            {
+                cv.Optional(CONF_BRIGHTNESS, default=1.0): cv.percentage,
+                cv.Optional(CONF_RED, default=0): cv.percentage,
+                cv.Optional(CONF_GREEN, default=0): cv.percentage,
+                cv.Optional(CONF_BLUE, default=0): cv.percentage,
+                cv.Optional(CONF_WHITE, default=0): cv.percentage,
+            }
+           ),
     },
 )
 async def addressable_stars_effect_to_code(config, effect_id):
     var = cg.new_Pvariable(effect_id, config[CONF_NAME])
     cg.add(var.set_stars_probability(config[CONF_STARS_PROBABILITY]))
+    color = cg.StructInitializer(
+                AddressableColorStarsEffectColor,
+                ("r", int(round(config[CONF_RED] * 255))),
+                ("g", int(round(config[CONF_GREEN] * 255))),
+                ("b", int(round(config[CONF_BLUE] * 255))),
+                ("w", int(round(config[CONF_WHITE] * 255))),
+                ("brightness", int(round(config[CONF_BRIGHTNESS] * 100))),
+            )
+    cg.add(var.set_color(color))
     return var
 
 @register_addressable_effect(
--- components/custom-addressable-effetcs/addressable_stars_effect.h
+++ components/custom-addressable-effetcs/addressable_stars_effect.h
@@ -14,6 +14,11 @@
 namespace esphome {
 namespace light {
 
+struct AddressableColorStarsEffectColor {
+  uint8_t r, g, b, w;
+  uint8_t brightness;
+};
+
 class AddressableStarsEffect : public AddressableLightEffect {
  public:
   explicit AddressableStarsEffect(const std::string &name) : AddressableLightEffect(name) {}
@@ -21,19 +26,27 @@ class AddressableStarsEffect : public AddressableLightEffect {
     auto &it = *this->get_addressable_();
     it.all() = Color::BLACK;
     it.schedule_show(); 
+
+    auto r = this->color_effect_.r * this->color_effect_.brightness /100;
+    auto g = this->color_effect_.g * this->color_effect_.brightness /100;
+    auto b = this->color_effect_.b * this->color_effect_.brightness /100;
+    auto w = this->color_effect_.w * this->color_effect_.brightness /100;  
+    this->color_ = Color(r, g, b, w);
   }
   
   void apply(AddressableLight &it, const Color &current_color) override {
     for (auto view : it) {     
+        Color effect_color = (this->color_ == Color::BLACK ? current_color : this->color_);
+
         if (view.get_effect_data()==0 && (random_float() * 500 < this->stars_probability_)){
               view.set_effect_data(255);
         } 
         if (view.get_effect_data() > 0) {
             float intensit = -1*pow(view.get_effect_data()/180.1,2);
-            view = Color(current_color.r * exp(intensit),
-                          current_color.g * exp(intensit),
-                          current_color.b * exp(intensit),
-                          current_color.w * exp(intensit));
+            view = Color(effect_color.r * exp(intensit),
+                         effect_color.g * exp(intensit),
+                         effect_color.b * exp(intensit),
+                         effect_color.w * exp(intensit));
         } else {
             view = Color::BLACK;
         }
@@ -48,10 +61,12 @@ class AddressableStarsEffect : public AddressableLightEffect {
   }
 
   void set_stars_probability(float stars_probability) { this->stars_probability_ = stars_probability; }
+  void set_color(const AddressableColorStarsEffectColor &color) { this->color_effect_ = color; }
 
  protected:
   float stars_probability_{0.3};
-
+  AddressableColorStarsEffectColor color_effect_;
+  Color color_;
 
 };
 
