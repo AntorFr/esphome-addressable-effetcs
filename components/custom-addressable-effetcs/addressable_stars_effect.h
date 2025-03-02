64d2aea8603fda01ffda7ae0871626cb9c8500f3
AntorFR <contact@antor.fr>
2025-03-02 15:03:24 +0100

    Inits

--- components/custom-addressable-effetcs/addressable_stars_effect.h
+++ components/custom-addressable-effetcs/addressable_stars_effect.h
@@ -24,13 +24,19 @@ class AddressableStarsEffect : public AddressableLightEffect {
   }
   
   void apply(AddressableLight &it, const Color &current_color) override {
-    for (auto view : it) {     
-        Color effect_color = (this->color_.is_on() ? this->color_ : current_color);
+    for (auto view : it) {   
 
         if (view.get_effect_data()==0 && (random_float() * 500 < this->stars_probability_)){
               view.set_effect_data(255);
         } 
         if (view.get_effect_data() > 0) {
+            if (this->random_color_) {  
+              const Color effect_color = Color::random_color();
+            } else if (this->color_.is_on()) {
+              const Color effect_color = this->color_;
+            } else {
+              const Color effect_color = current_color;
+            }
             float intensit = -1*pow(view.get_effect_data()/180.1,2);
             view = Color(effect_color.r * exp(intensit),
                          effect_color.g * exp(intensit),
@@ -51,10 +57,12 @@ class AddressableStarsEffect : public AddressableLightEffect {
 
   void set_stars_probability(float stars_probability) { this->stars_probability_ = stars_probability; }
   void set_color(const Color &color) { this->color_ = color; }
+  void set_random_color(bool random) { this->random_color_ = random; }
 
  protected:
   float stars_probability_{0.3};
   Color color_;
+  bool random_color_{false};
 
 };
 
